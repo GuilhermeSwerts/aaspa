468b1bc2b71aa83bd8f72e549e89d5b9abf94006
Guilherme Swerts de Oliveira <p.guilherme.swerts@viveo.com.br>
2024-06-05 21:10:49 -0300

    .

--- AASPA/ClientApp/src/views/Cliente/clienteForm.jsx
+++ AASPA/ClientApp/src/views/Cliente/clienteForm.jsx
@@ -13,22 +13,23 @@ function ClienteForm({
         <form onSubmit={e => { e.preventDefault(); onSubmit() }} style={{ padding: 20 }}>
             <button type='button' className='btn btn-link' onClick={() => window.location.href = '/'}>Voltar</button>
             <h4>Dados do Cliente:</h4>
+            <small style={{ color: 'red', textAlign: 'center' }}>Campos com * são obrigatórios</small>
             <Row>
                 <Col md={4}>
                     <FormGroup>
-                        <Label for="cpf">CPF</Label>
+                        <Label for="cpf">CPF*</Label>
                         <Input required type="text" maxLength={14} name="cpf" id="cpf" value={Mascara.cpf(cliente.cpf)} onChange={handleChange} />
                     </FormGroup>
                 </Col>
                 <Col md={4}>
                     <FormGroup>
-                        <Label for="nome">Nome</Label>
+                        <Label for="nome">Nome*</Label>
                         <Input required type="text" name="nome" id="nome" value={cliente.nome} onChange={handleChange} />
                     </FormGroup>
                 </Col>
                 <Col md={4}>
                     <FormGroup>
-                        <Label for="email">E-mail</Label>
+                        <Label for="email">E-mail*</Label>
                         <Input required type="email" name="email" id="email" value={cliente.email} onChange={handleChange} />
                     </FormGroup>
                 </Col>
@@ -36,19 +37,19 @@ function ClienteForm({
             <Row>
                 <Col md={2}>
                     <FormGroup>
-                        <Label for="nome">CEP</Label>
+                        <Label for="nome">CEP*</Label>
                         <Input required maxLength={9} type="text" name="cep" id="cep" value={Mascara.cep(cliente.cep)} onChange={e => { handleChange(e); getLogadouro(e) }} />
                     </FormGroup>
                 </Col>
                 <Col md={6}>
                     <FormGroup>
-                        <Label for="endereco">Logradouro</Label>
+                        <Label for="endereco">Logradouro*</Label>
                         <Input required type="text" name="logradouro" id="logradouro" value={cliente.logradouro} onChange={handleChange} />
                     </FormGroup>
                 </Col>
                 <Col md={3}>
                     <FormGroup>
-                        <Label for="nome">Complemento</Label>
+                        <Label for="nome">Complemento*</Label>
                         <Input required type="text" name="complemento" id="complemento" value={cliente.complemento} onChange={handleChange} />
                     </FormGroup>
                 </Col>
@@ -56,25 +57,25 @@ function ClienteForm({
             <Row>
                 <Col md={5}>
                     <FormGroup>
-                        <Label for="nome">localidade</Label>
+                        <Label for="nome">localidade*</Label>
                         <Input required type="text" name="localidade" id="localidade" value={cliente.localidade} onChange={handleChange} />
                     </FormGroup>
                 </Col>
                 <Col md={2}>
                     <FormGroup>
-                        <Label for="endereco">Bairro</Label>
+                        <Label for="endereco">Bairro*</Label>
                         <Input required type="text" name="bairro" id="bairro" value={cliente.bairro} onChange={handleChange} />
                     </FormGroup>
                 </Col>
                 <Col md={2}>
                     <FormGroup>
-                        <Label for="endereco">Uf</Label>
+                        <Label for="endereco">Uf*</Label>
                         <Input required type="text" name="uf" id="uf" value={cliente.uf} onChange={handleChange} />
                     </FormGroup>
                 </Col>
                 <Col md={3}>
                     <FormGroup>
-                        <Label for="endereco">Numero</Label>
+                        <Label for="endereco">Numero*</Label>
                         <Input required maxLength={5} type="text" name="numero" id="numero" value={cliente.numero} onChange={handleChange} />
                     </FormGroup>
                 </Col>
@@ -82,8 +83,8 @@ function ClienteForm({
             <Row>
                 <Col md={3}>
                     <FormGroup>
-                        <Label for="dataNascimento">Data de Nascimento</Label>
-                        <Input type="date" name="dataNasc" id="dataNasc" value={cliente.dataNasc} onChange={handleChange} />
+                        <Label for="dataNascimento">Data de Nascimento*</Label>
+                        <Input required type="date" name="dataNasc" id="dataNasc" value={cliente.dataNasc} onChange={handleChange} />
                     </FormGroup>
                 </Col>
                 <Col md={3}>
@@ -114,8 +115,8 @@ function ClienteForm({
                 </Col>
                 <Col md={4}>
                     <FormGroup>
-                        <Label for="telefoneCelular">Telefone Celular</Label>
-                        <Input type="text" name="telefoneCelular" maxLength={15} id="telefoneCelular" value={Mascara.telefone(cliente.telefoneCelular)} onChange={handleChange} />
+                        <Label for="telefoneCelular">Telefone Celular*</Label>
+                        <Input required type="text" name="telefoneCelular" maxLength={15} id="telefoneCelular" value={Mascara.telefone(cliente.telefoneCelular)} onChange={handleChange} />
                     </FormGroup>
                 </Col>
                 <Col md={4}>
@@ -154,14 +155,14 @@ function ClienteForm({
             <Row>
                 <Col md={4}>
                     <FormGroup>
-                        <Label for="CPF/CNPJ">CPF/CNPJ</Label>
-                        <Input maxLength={18} type="text" name='cpfOuCnpj' onChange={handleChangeCaptador} value={captador.cpfOuCnpj.length > 14 ? Mascara.cnpj(captador.cpfOuCnpj) : Mascara.cpf(captador.cpfOuCnpj)} />
+                        <Label for="CPF/CNPJ">CPF/CNPJ*</Label>
+                        <Input required maxLength={18} type="text" name='cpfOuCnpj' onChange={handleChangeCaptador} value={captador.cpfOuCnpj.length > 14 ? Mascara.cnpj(captador.cpfOuCnpj) : Mascara.cpf(captador.cpfOuCnpj)} />
                     </FormGroup>
                 </Col>
                 <Col md={4}>
                     <FormGroup>
-                        <Label for="nome">Nome</Label>
-                        <Input maxLength={255} type="text" name="nome" id="nome" value={captador.nome} onChange={handleChangeCaptador} />
+                        <Label for="nome">Nome*</Label>
+                        <Input required maxLength={255} type="text" name="nome" id="nome" value={captador.nome} onChange={handleChangeCaptador} />
                     </FormGroup>
                 </Col>
             </Row>
