8994d2df531d62a30b5b6400d386dfd4fbfe7df5
Jorge Fernandes <150183896+jorge-datawualify@users.noreply.github.com>
2024-06-13 19:14:40 -0300

    ok

--- AASPA.Models/Requests/NovoClienteRequest.cs
+++ AASPA.Models/Requests/NovoClienteRequest.cs
@@ -23,25 +23,25 @@ namespace AASPA.Models.Requests
         public string Localidade { get; set; }
         public string Uf { get; set; }
         public string Numero { get; set; }
-        public string Complemento { get; set; }
+        public string Complemento { get; set; } = string.Empty;
         public DateTime DataNasc { get; set; }
-        public string NrDocto { get; set; }
-        public string Empregador { get; set; }
+        public string NrDocto { get; set; } = string.Empty;
+        public string Empregador { get; set; } = string.Empty;
         public string MatriculaBeneficio { get; set; }
         public string NomeMae { get; set; }
-        public string NomePai { get; set; }
-        public string TelefoneFixo { get; set; }
+        public string NomePai { get; set; } = string.Empty;
+        public string TelefoneFixo { get; set; } = string.Empty;
         public string TelefoneCelular { get; set; }
         public bool PossuiWhatsapp { get; set; }
         public string FuncaoAASPA { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
     }
 
     public class NovoCaptador 
     {
         public string CpfOuCnpj { get; set; }
         public string Nome { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao { get; set; } = string.Empty;
     }
 
 }
