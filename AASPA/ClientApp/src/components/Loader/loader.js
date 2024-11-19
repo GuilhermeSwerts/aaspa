fb378677966231f5db024ee9fc1da536c9ddc8d5
Guilherme <guilh@DESKTOP-CED4TVC>
2024-11-19 19:14:48 -0300

    img do loader

--- AASPA/ClientApp/src/components/Loader/loader.js
+++ AASPA/ClientApp/src/components/Loader/loader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './loader.css';
 import { FaGear } from 'react-icons/fa6';
-import Logo from '../../assets/logo.png'
+import Logo from '../../assets/Logo.png'
 
 class Loader extends React.Component {
     constructor(props) {
